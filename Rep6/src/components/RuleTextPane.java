863abcc5514366e2a884445471ead662a737e654
TopGear <tglover.nuppy@gmail.com>
2014-12-04 03:35:08 +0900

    適当

--- Rep6/src/components/RuleTextPane.java
+++ Rep6/src/components/RuleTextPane.java
@@ -3,7 +3,6 @@ package components;
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.Enumeration;
 
 import javax.swing.JTextPane;
 import javax.swing.event.DocumentEvent;
@@ -17,6 +16,7 @@ import javax.swing.text.Document;
 import javax.swing.undo.CannotRedoException;
 import javax.swing.undo.CannotUndoException;
 import javax.swing.undo.CompoundEdit;
+import javax.swing.undo.UndoManager;
 import javax.swing.undo.UndoableEdit;
 
 /**
@@ -98,95 +98,73 @@ public class RuleTextPane extends JTextPane implements DocumentListener, KeyList
 	/**
 	 * A customized UndoManager
 	 */
-	protected class RuleUndoManager extends CompoundEdit {
-		private Document document = null;
-		private boolean undoneOrRedone = true;
+	protected class RuleUndoManager extends UndoManager {
+		private boolean nextIsMergeable = false;
 		
 		@Override
 		public synchronized boolean addEdit(UndoableEdit anEdit) {
 			final UndoableEdit lastEdit = lastEdit();
 			if (anEdit instanceof AbstractDocument.DefaultDocumentEvent) {
-				final EventType lastEditType = (lastEdit == null) ? null : ((AbstractDocument.DefaultDocumentEvent) ((RuleUndoableEdit) lastEdit).delegate).getType();
+				final EventType lastEditType = (lastEdit == null) ? null : ((MultiEdit) lastEdit).lastEventType;
 				final EventType theEditType = ((AbstractDocument.DefaultDocumentEvent) anEdit).getType();
 				
-				RuleUndoableEdit theEdit = new RuleUndoableEdit(anEdit);
 				boolean shouldMerge = false;
-				
+
 				// 連続した入力、変更、削除であれば直前の編集にマージ
 				if (theEditType == lastEditType) {
 					shouldMerge = true;
 				}
 				
-				// ドキュメントが変わったら significant
-				if (document != getDocument()) {
-					document = getDocument();
-					shouldMerge = false;
-				}
-				
-				// 最後に undo, redo したなら significant
-				if (undoneOrRedone) {
-					undoneOrRedone = false;
-					shouldMerge = false;
-				}
+				shouldMerge = shouldMerge && nextIsMergeable;
+				System.out.println("shouldMerge = "+shouldMerge);
 
-				if (shouldMerge) {
-					theEdit.isSignificant = false;
+				nextIsMergeable = true;
+				if (shouldMerge && lastEdit != null) {
+					return lastEdit.addEdit(anEdit);
+				} else {
+					if (lastEdit != null) {
+						((MultiEdit) lastEdit).end();
+					}
+					return super.addEdit(new MultiEdit(anEdit));
 				}
-				return super.addEdit(theEdit);
 			} else {
-				return super.addEdit(anEdit);
+				return false;
 			}
 		}
 		
 		@Override
 		public synchronized void undo() throws CannotUndoException {
 			super.undo();
-			int i = edits.size();
-			while (i-- > 0) {
-				UndoableEdit e = edits.elementAt(i);
-				e.undo();
-			}
-			undoneOrRedone = true;
+			nextIsMergeable = false;
 		}
-		
+
 		@Override
 		public synchronized void redo() throws CannotRedoException {
 			super.redo();
-			Enumeration<UndoableEdit> cursor = edits.elements();
-			while (cursor.hasMoreElements()) {
-				cursor.nextElement().redo();
-			}
-			undoneOrRedone = true;
+			nextIsMergeable = false;
 		}
 
-		private void setLastEditSignificant(boolean isSignificant) {
-			final UndoableEdit lastEdit = lastEdit();
-			if (lastEdit != null && lastEdit instanceof RuleUndoableEdit) {
-				((RuleUndoableEdit) lastEdit).isSignificant = isSignificant;
+		protected class MultiEdit extends CompoundEdit {
+			EventType lastEventType;
+			
+			public MultiEdit(UndoableEdit anEdit) {
+				super();
+				addEdit(anEdit);
 			}
-		}
+			
+			@Override
+			public boolean addEdit(UndoableEdit anEdit) {
+				if (anEdit instanceof AbstractDocument.DefaultDocumentEvent) {
+					lastEventType = ((AbstractDocument.DefaultDocumentEvent) anEdit).getType();
+				}
+				return super.addEdit(anEdit);
+			}
 
-		/**
-		 * かっこ悪いけどこんな書き方しか思いつかなかった
-		 */
-		protected class RuleUndoableEdit implements UndoableEdit {
-			private UndoableEdit delegate;
-			private boolean isSignificant;
-			public RuleUndoableEdit(UndoableEdit delegate) {
-				this.delegate = delegate;
-				this.isSignificant = true;
+			@Override
+			public boolean isInProgress() {
+				// canUndo(), canRedo() が常に false になるのを防ぐ
+				return false;
 			}
-			@Override public void undo() throws CannotUndoException { delegate.undo(); }
-			@Override public boolean canUndo() { return delegate.canUndo(); }
-			@Override public void redo() throws CannotRedoException { delegate.redo(); }
-			@Override public boolean canRedo() { return delegate.canRedo(); }
-			@Override public void die() { delegate.die(); }
-			@Override public boolean addEdit(UndoableEdit anEdit) { return delegate.addEdit(anEdit); }
-			@Override public boolean replaceEdit(UndoableEdit anEdit) { return delegate.replaceEdit(anEdit); }
-			@Override public boolean isSignificant() { return isSignificant; } // only this method is modified
-			@Override public String getPresentationName() { return delegate.getPresentationName(); }
-			@Override public String getUndoPresentationName() { return delegate.getUndoPresentationName(); }
-			@Override public String getRedoPresentationName() { return delegate.getRedoPresentationName(); }
 		}
 	}
 }
